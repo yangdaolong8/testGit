fa1a43fbb3e11c483407c06d01281efedb9f2cfd
yangdaolong8 <yangdaolong8@163.com>
2018-03-29 17:01:18 +0800

    提交到本地库

--- testVersion/src/main/java/com/example/myapplication/MainActivity.java
+++ testVersion/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,4 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * 初始化view
+     */
+    private void initView() {
+
+    }
 }

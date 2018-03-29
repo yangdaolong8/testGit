cc3b880e0621f2702c5e1485ef25c0272773c767
yangdaolong8 <yangdaolong8@163.com>
2018-03-29 17:40:45 +0800

    app

--- testVersion/src/main/java/com/example/myapplication/MainActivity.java
+++ testVersion/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,5 @@ public class MainActivity extends AppCompatActivity {
      * 初始化view
      */
     private void initView() {
-
     }
 }
